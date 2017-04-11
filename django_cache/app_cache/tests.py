bae73026bef8e8a35ba8612e90f759943dee3399
myy <mengyy_linux@163.com>
2017-04-11 17:22:53 +0800

    Add testcase.

--- django_cache/app_cache/tests.py
+++ django_cache/app_cache/tests.py
@@ -1,3 +1,27 @@
+# -*- coding: utf-8 -*-
+
+import requests
+import nose
+from nose import with_setup
+
 from django.test import TestCase
 
-# Create your tests here.
+
+domain = 'http://localhost:8000'
+
+def setup_module():
+    pass
+
+def teardown_module():
+    pass
+
+def setup():
+    pass
+
+def teardown():
+    pass
+
+def test_session():
+    url = '{domain}/cache/session/'.format(domain=domain)
+    resp = requests.get(url)
+    assert(resp.status_code==200)
